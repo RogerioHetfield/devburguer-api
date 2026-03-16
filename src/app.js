aeb091c099e1c7dc0c0175de602d579695bee620
RogerioHetfield <rgfm.95@gmail.com>
2026-03-16 15:26:44 -0300

    CorsAtt

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ import 'dotenv/config';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://restaurantesaborecor.netlify.app"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/product/file', fileRouteConfig);
